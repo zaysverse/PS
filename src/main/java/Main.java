38b71df8d9599ee8cb343e826dd5a936188f8267
zaysverse <zeze@jbnu.ac.kr>
2023-08-30 01:42:35 +0900

    P11054 LIS 알고리즘 첫 골드 문제 패스! 풀이 다 해놓고 마지막에 중복체크하는 부분에서 혼란이 있었지만 수정 후 뜬 맞았습니다!! 우하하 !

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,40 +1,56 @@
+
+/* 11054 바이토닉 수열 : 증가 부분수열 + 감소 부분수열 로 이루어져있음
+LIS 알고리즘
+dp1, dp2 - 각각 증가([0]~[n-1]), 감소([n-1]~[0])하는 구간  부분수열의 길이를 구함
+Math.max(dp1[i]+dp2[i+1], 현재값)
+ */
+
 import java.util.Scanner;
 
-/* 11722 가장 긴 감소하는 부분 수열
-LIS 알고리즘!
-input : seq[N]
-- dp[N] : memorize 부분
- */
 public class Main {
-    static Integer[] seq, dp;
+    static Integer[] seq, dpIncrease, dpDecrease;
+    static int N;
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int N = sc.nextInt();
+        N = sc.nextInt();
         seq = new Integer[N];
-        dp = new Integer[N];
+        dpIncrease = new Integer[N];
+        dpDecrease = new Integer[N];
 
+        // input
         for (int i = 0; i < N; i++) {
             seq[i] = sc.nextInt();
         }
 
         int result = 0;
-        for (int i = 0; i < N; i++) {
-            result = Math.max(result, LIS(i));
+        for (int i = 0; i < N ; i++) {
+            result = Math.max(result, LIS(i) + LDS(i));
         }
-        System.out.println(result);
-
+        System.out.println(--result);       // dp[i]부분이 중복되었으므로 -1
     }
 
     static int LIS(int n) {
-        if (dp[n] == null) {
-            dp[n] = 1;
+        if (dpIncrease[n] == null) {
+            dpIncrease[n] = 1;
             for (int i = n - 1; i >= 0; i--) {
-                if (seq[i] > seq[n]) {
-                    dp[n] = Math.max(dp[n], LIS(i) + 1);
-                }
+                if (seq[i] < seq[n])
+                    dpIncrease[n] = Math.max(dpIncrease[n], LIS(i) + 1);
             }
         }
-        return dp[n];
+        return dpIncrease[n];
     }
+
+    static int LDS(int n) {
+        if (dpDecrease[n] == null) {
+            dpDecrease[n] = 1;
+            for (int i = n + 1; i <= N - 1; i++) {
+                if (seq[i] < seq[n])
+                    dpDecrease[n] = Math.max(dpDecrease[n], LDS(i) + 1);
+            }
+        }
+        return dpDecrease[n];
+    }
+
+
 }
