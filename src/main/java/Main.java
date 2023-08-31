74d5f1aa3adfcd2d98e2554a2ba5e87c7bd3914e
zaysverse <zeze@jbnu.ac.kr>
2023-08-31 15:28:24 +0900

    P1699 출력은 맞지만 이건 이렇게 푸는 게 아니다.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,47 +1,31 @@
+/* 1699 제곱수의 합
+input : int n
+1 = 1
+2 = 1 + 1
+3 = 1 + 1 + 1
+4 = 2
+5 = 2 + 1
+*/
 
-/* 2579 계단 오르기
-- 마지막 계단은 반드시 포함.
-- 세개 연속은 불가능. 한 개는 점프 가능.
-
-    10 20 15 25 10 20
-    o  o  x             XXXXXXXXXXXXXXXXXXXXX 마지막 발판을 밟는 경우를 제한해서 메모이제이션
-    o  x  o         dp[n] = dp[n-2] + seq[n]
- o  x  o  o         dp[n] = dp[n-3] + seq[n-1] + seq[n]
-
-dp[n]: n번째 발판을 밟은, 최대점수
-
- */
-
+import java.sql.PreparedStatement;
 import java.util.Scanner;
 
 public class Main {
-    static Integer[] seq, dp;
+    static int result;
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int N = sc.nextInt();       // 계단의 개수
-        seq = new Integer[N + 1];
-        dp = new Integer[N + 1];
-        for (int i = 1; i <= N; i++) {
-            seq[i] = sc.nextInt();
-        }
-
-        dp[0] = seq[0] = 0;
-        dp[1] = seq[1];
-
-        if (N >= 2) {
-            dp[2] = seq[1] + seq[2];
-        }
-
-        System.out.println(recur(N));
-
+        long N = sc.nextInt();
+        recur(N);
+        System.out.println(result);
     }
 
-    static int recur(int n) {
-        if (dp[n] == null) {
-            dp[n] = Math.max(recur(n - 2), recur(n - 3) + seq[n - 1]) + seq[n];
+    static void recur(long n) {
+        long sqrt = (long) Math.sqrt(n);
+        result++;
+        if (n - sqrt * sqrt != 0) {
+            recur(n - sqrt * sqrt);
         }
 
-        return dp[n];
     }
 }
