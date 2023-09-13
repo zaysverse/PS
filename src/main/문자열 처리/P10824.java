1e049d6310877f8d608ebe3ec0db3e2ab1536466
zaysverse <zeze@jbnu.ac.kr>
2023-09-13 18:13:18 +0900

    P11656 접미사  문자열 S 의 접미사는 substring(i, S.length()-1)  정렬 : sort (사전순)

--- src/main/문자열 처리/P10824.java
+++ src/main/문자열 처리/P10824.java
@@ -1,12 +1,26 @@
 import java.io.*;
+import java.util.Arrays;
 import java.util.StringTokenizer;
+
+/* 11656 접미사
+문자열 S 의 접미사는 substring(i, S.length()-1)
+정렬 : sort (사전순)
+ */
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        String AB = st.nextToken()+st.nextToken();
-        String CD = st.nextToken()+st.nextToken();
-        System.out.println(Long.parseLong(AB)+Long.parseLong(CD));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        String S = br.readLine();
+        String[] ans = new String[S.length()];
+        for (int i = 0; i < S.length(); i++) {
+            ans[i] = S.substring(i);
+        }
+        Arrays.sort(ans);
+        for (String a : ans) {
+            bw.write(a + "\n");
+        };
+        bw.flush();
+        bw.close();
     }
 }
