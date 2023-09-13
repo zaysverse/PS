7da392501c232643ff4557a6f8cc71510c3b34b7
zaysverse <zeze@jbnu.ac.kr>
2023-09-13 17:39:11 +0900

    P10820 아스키코드를 이용하면 간단하지만 자세한 아스키 수가 무엇인지 기억이 안날 수도 있다. 그땐 Character 클래스의 함수들을 이용하도록 한다. - isUpperCase() - isLowerCase() - isDigit()

--- src/main/문자열 처리/P10820.java
+++ src/main/문자열 처리/P10820.java
@@ -12,11 +12,16 @@ public class Main {
             if(str==null) break;
             int[] cnt = new int[4];
             for (int j = 0; j < str.length(); j++) {
-                char cur = str.charAt(j);
-                if (cur == ' ') cnt[3]++;  //공백인지 확인
-                if (Character.isUpperCase(cur)) cnt[1]++;  //대문자인지 확인
-                if (Character.isLowerCase(cur)) cnt[0]++;  //소문자인지 확인
-                if (Character.isDigit(cur)) cnt[2]++;  //숫자인지 확인
+                int cur = (int) str.charAt(j);
+                if (cur >= 97 && cur < 123) {
+                    cnt[0]++;   // 소문자 개수 + 1
+                } else if (cur >= 65 && cur < 97) {
+                    cnt[1]++;   // 대문자 개수 + 1
+                } else if (cur >= 48 && cur <= 57) {
+                    cnt[2]++;
+                } else if (cur==32){
+                    cnt[3]++;
+                }
             }
             for (int count : cnt) {
                 System.out.print(count+" ");
